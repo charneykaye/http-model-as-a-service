36c1b09286489ccbd88e2177372b9647d22c1281
Nick Kaye <nick.c.kaye@gmail.com>
2014-11-09 16:30:58 -0500

    Scaffold Angular-Fullstack JavaScript web application with Yeoman. #1

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/httpmodelasaservice-dev'
+  },
+
+  seedDB: true
+};
