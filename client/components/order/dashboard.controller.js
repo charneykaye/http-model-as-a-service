67ef443557630a4fc01fbc66e7e21d951779aaa3
Nick Kaye <nick.c.kaye@gmail.com>
2014-11-15 21:30:09 -0500

    Documentation #2

--- client/components/order/dashboard.controller.js
+++ client/components/order/dashboard.controller.js
@@ -1,12 +1,13 @@
 /**
  *
+ * Using $http Model-as-a-Service in AngularJS
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @repository https://github.com/nickckaye/http-model-as-a-service
+ *
  * Simple AngularJS Example Project to demo the philosophy behind contemporary
  * front-end construction, specifically separating out concerns of a "model",
  * relying for our data models entirely on a JSON API.
  *
- * Using $http Model-as-a-Service in AngularJS
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- * @repository https://github.com/nickckaye/http-model-as-a-service
  * @typedef {angular.controller} OrderDashboardCtrl
  */
 angular.module('httpModelAsAServiceApp')
--- client/components/order/order.service.js
+++ client/components/order/order.service.js
@@ -1,14 +1,20 @@
 /**
  *
+ * Using $http Model-as-a-Service in AngularJS
+ * @author Nick Kaye <nick.c.kaye@gmail.com>
+ * @repository https://github.com/nickckaye/http-model-as-a-service
+ *
+ * Simple AngularJS Example Project to demo the philosophy behind contemporary
+ * front-end construction, specifically separating out concerns of a "model",
+ * relying for our data models entirely on a JSON API.
+ *
+ * @typedef {angular.service} OrderService
+ *
  * OrderService Model-as-a-Service
  *
  * REST methods return Angular Promises
  * @typedef {angular.promise} HttpPromise
  *
- * Using $http Model-as-a-Service in AngularJS
- * @author Nick Kaye <nick.c.kaye@gmail.com>
- * @repository https://github.com/nickckaye/http-model-as-a-service
- * @typedef {angular.service} OrderService
  */
 angular.module('httpModelAsAServiceApp').service('OrderService', function ($http, $q) {
   'use strict';
