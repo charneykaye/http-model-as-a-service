d4e5df42f0eb9fd5da24998ddf8a656d8defc403
Nick Kaye <nick.c.kaye@gmail.com>
2014-11-16 23:43:38 -0500

    "View an Order" retrieves record via Service; updates U.I. #8

--- client/components/order/editor/editor.controller.spec.js
+++ client/components/order/editor/editor.controller.spec.js
@@ -7,52 +7,13 @@
  * Simple AngularJS Example Project to demo the philosophy behind contemporary
  * front-end construction, specifically separating out concerns of a "model",
  * relying for our data models entirely on a JSON API.
- *
- * Client Tests implement Karma + Jasmine
  */
-describe('Controller: OrderEditorCtrl', function () {
-  'use strict';
-  var OrderEditorCtrl
-    , scope
-    , $httpBackend
-    ;
-
-  // load the main module
-  beforeEach(module('httpModelAsAServiceApp'));
-
-  // inject backend
-  beforeEach(inject(function (_$httpBackend_, $controller, $rootScope) {
-    $httpBackend = _$httpBackend_;
-    $httpBackend.expectGET('/api/orders')
-      .respond(test_order_index);
-    scope = $rootScope.$new();
-    OrderEditorCtrl = $controller('OrderEditorCtrl', {
-      $scope: scope
-    });
-  }));
-
-  it('should attach a list of orders to the scope', function () {
-    $httpBackend.flush();
-    expect(scope.list_of_orders.length).toBe(test_order_index.length);
-  });
-
-  it('should create a new order', function () {
-    $httpBackend.flush();
-    $httpBackend.expectPOST('/api/orders', test_new_order)
-      .respond(test_new_order);
-    $httpBackend.expectGET('/api/orders')
-      .respond(test_order_index);
-    scope.create(test_new_order);
-    $httpBackend.flush();
-  });
-
-});
 
 /**
  * Test data
  * @type {*}
  */
-var test_order_one = {
+var test_order = {
     "purchase_number": 100,
     "created_at": "2014-11-15T23:58:48.333Z",
     "name": "ovni",
@@ -61,37 +22,60 @@ var test_order_one = {
     "_id": "5467e8b88a4f0313533ef420",
     "__v": 0
   }
-  , test_order_two = {
-    "purchase_number": 101,
-    "created_at": "2014-11-15T23:58:58.358Z",
-    "name": "fi",
-    "info": "Vamgiud jokra ahkudwu jicavu tez on cak ji ziccepnar ma obdo wuk erwabus zow ohupu. Tuadipo wo ah jus okweko vugiwon wihufuce ti demacoj salla zu afkiwove wubdak uk cozubu ko buto. Afotuspol zu al vehfaim olmitec ajsiczu ket ojiaf vavta pub jisior pucecek gecfuf mod nucrew. Bo otlo iwevud ikaob bofciaju gajawwu be pazaz mig ga imo usi wufwor tatefa nessuj jijlipot. Co gas gij diutjo civ zetlaeze looje cej zov segder wivi ufmemza kecesnes gawul jibcu ojuiw voketofi.",
-    "amount": 125,
-    "_id": "5467e8c28a4f0313533ef421",
-    "__v": 0
-  }
-  , test_order_three = {
-    "purchase_number": 102,
-    "created_at": "2014-11-15T23:59:08.355Z",
-    "name": "visow",
-    "info": "Ov bueka woaf sasjo ov cul dojnamdoh nuil ilwikzos raite zubatfe mu dugij. Awututmer wow omesa hianuwi vupwa irapowi hicwobi suko hipot ma rumulamu uw. Ehemug eha vecwok degmobsav ni hun ohhuglaj ag edge fenah hotnudef gupnar ra jewopzi mil. Hojtal manwivmig vodpad pepemuh izejale car vuftahkob cebudel ce wijimuw ohofusur kaccoze wuhbob udeicvus. Doof geufmu ubicun husija nose wuvu aj esedoheki wevizki fufumi misva pupjalud dagvu uvo az.",
-    "amount": 159,
-    "_id": "5467e8cc8a4f0313533ef422",
-    "__v": 0
-  }
-  , test_new_order = {
-    "purchase_number": 109,
-    "created_at": "2014-11-15T23:59:08.359Z",
-    "name": "fjjwoe",
-    "info": "Nuil ilwikzos raite zubatfe mu dugij ov bueka woaf sasjo ov cul dojnamdoh. Nicwobi suko hipot ma rumulamu uw. Ehemug eha vecwok degmobsav ni hun ohhuglaj ag edge fenah hotnudef gupnar ra jewopzi mil. Hojtal manwivmig vodpad pepemuh izejale car vuftahkob cebudel ce wijimuw ohofusur kaccoze wuvu aj esedoheki wevizki fufumi misva pupjalud dagvu uvo az wuhbob udeicvus. Doof geufmu ubicun husija nose.",
-    "amount": 23,
-    "_id": "563786c48aef0d31c53ef422",
-    "__v": 0
-  }
-  , test_order_index = [
-    test_order_one,
-    test_order_two,
-    test_order_three
-  ]
   ;
 
+/**
+ * Client Tests implement Karma + Jasmine
+ */
+describe('Controller: OrderEditorCtrl', function () {
+  'use strict';
+  var OrderEditorCtrl
+    , scope
+    , mockOrderService
+    ;
+
+  // load the main module with a mocked Order Service
+  beforeEach(module("httpModelAsAServiceApp", function ($provide) {
+    mockOrderService = {
+      create: function () {
+        // something
+      },
+      show: function () {
+        // something
+      },
+      update: function () {
+        // something
+      },
+      destroy: function () {
+        // something
+      }
+    };
+    $provide.value("OrderService", mockOrderService);
+  }));
+
+  // inject controller
+  beforeEach(inject(function ($controller, $rootScope) {
+    scope = $rootScope.$new();
+    OrderEditorCtrl = $controller('OrderEditorCtrl', {
+      $scope: scope
+    });
+  }));
+
+  it('should Create a new Order via the Service', function () {
+
+  });
+
+  it('should Show an Order via the Service', function () {
+
+  });
+
+  it('should Update an Order via the Service', function () {
+
+  });
+
+  it('should Delete an Order via the Service', function () {
+
+  });
+
+})
+;
